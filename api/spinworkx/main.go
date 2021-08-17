f63faadcb52a4846057391cc05600a38c1fbd308
kev-inc <kevinchanxy@gmail.com>
2021-08-17 16:18:00 +0000

    moved

--- api/spinworkx/main.go
+++ api/spinworkx/main.go
@@ -1,19 +1,21 @@
 package handler
 
 import (
-    "strconv"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
-func GetMain(w http.ResponseWriter, r *http.Request) {	
+func GetMain(w http.ResponseWriter, r *http.Request) {
 	type Item struct {
-		Name	string  `json:"name"`
-		Price 	string 	`json:"price"`
-		Img    	string 	`json:"img"`
-		Href	string	`json:"href"`
+		Name  string `json:"name"`
+		Price string `json:"price"`
+		Img   string `json:"img"`
+		Href  string `json:"href"`
 	}
 	items := []Item{}
 	category := r.URL.Query().Get("category")
@@ -22,11 +24,13 @@ func GetMain(w http.ResponseWriter, r *http.Request) {
 	col := colly.NewCollector()
 	col.OnHTML("ul.products", func(e *colly.HTMLElement) {
 		e.ForEach("li > div.entry-product", func(_ int, el *colly.HTMLElement) {
+			href := el.ChildAttr("div.entry-featured > a", "href")
+			hrefSplit := strings.Split(href, "/")
 			item := Item{
-				Name: el.ChildText("div.entry-wrap > header > h3"),
+				Name:  el.ChildText("div.entry-wrap > header > h3"),
 				Price: el.ChildText("div.entry-wrap > header > span"),
-				Img: el.ChildAttr("div.entry-featured > a > img", "src"),
-				Href: el.ChildAttr("div.entry-featured > a", "href"),
+				Img:   el.ChildAttr("div.entry-featured > a > img", "src"),
+				Href:  hrefSplit[len(hrefSplit)-2],
 			}
 			items = append(items, item)
 		})
@@ -40,7 +44,7 @@ func GetMain(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, string(j))
 	})
 	col.OnError(func(r *colly.Response, err error) {
-		fmt.Fprintf(w, "Server responded with a " + strconv.Itoa(r.StatusCode))
+		fmt.Fprintf(w, "Server responded with a "+strconv.Itoa(r.StatusCode))
 	})
 	if category == "" {
 		if page == "" || page == "1" {
